ad8ce63f1658f9a62f90808ad2928bdc067528cd
James <j.mills@griffith.edu.au>
2013-01-02 10:29:08 +1000

    Added manager and watcher fixures from recent circuits dev

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,44 +4,97 @@
 
 """pytest config"""
 
-from os import path
-from time import sleep
-
 import pytest
 
+from os import path
+from time import sleep
+from collections import deque
 
+from circuits.net.sockets import Close
+from circuits.web import Controller, Server, Static
+from circuits import handler, BaseComponent, Component, Debugger, Manager
+
+TIMEOUT = 0.1
 DOCROOT = path.join(path.dirname(__file__), "docroot")
 
 
+class Watcher(BaseComponent):
+
+    def init(self):
+        self.events = deque([], 50)
+
+    @handler(channel="*", priority=999.9)
+    def _on_event(self, event, *args, **kwargs):
+        self.events.append(event)
+
+    def wait(self, name, channel=None, timeout=3.0):
+        for i in range(int(timeout / TIMEOUT)):
+            if channel is None:
+                for event in self.events:
+                    if event.name == name:
+                        return True
+            else:
+                for event in self.events:
+                    if event.name == name and channel in event.channels:
+                        return True
+            sleep(TIMEOUT)
+
+
+class Root(Controller):
+
+    def hello(self):
+        return "Hello World!"
+
+
+class WebApp(Component):
+
+    channel = "web"
+
+    def init(self, docroot=DOCROOT):
+        self.docroot = docroot
+
+        self.server = Server(0).register(self)
+        Root().register(self)
+        Static(docroot=self.docroot).register(self)
+
+
 @pytest.fixture(scope="session")
-def webapp(request):
-    from circuits import Component
-    from circuits.net.sockets import Close
-    from circuits.web import Controller, Server, Static
+def manager(request):
+    manager = Manager()
 
-    class Root(Controller):
+    def finalizer():
+        manager.stop()
 
-        def hello(self):
-            return "Hello World!"
+    request.addfinalizer(finalizer)
 
-    class WebApp(Component):
+    manager.start()
 
-        def init(self, docroot):
-            self.docroot = docroot
+    if request.config.option.verbose:
+        Debugger().register(manager)
 
-            self.server = Server(0).register(self)
-            Root().register(self)
-            Static(docroot=self.docroot).register(self)
+    return manager
 
-    webapp = WebApp(DOCROOT)
 
-    webapp.start()
-    sleep(1)  # Give circuits.web time to startup
+@pytest.fixture(scope="session")
+def watcher(request, manager):
+    watcher = Watcher().register(manager)
+
+    def finalizer():
+        watcher.unregister()
+
+    return watcher
+
+
+@pytest.fixture(scope="session")
+def webapp(request, manager, watcher):
+    webapp = WebApp().register(manager)
 
     def finalizer():
         webapp.fire(Close(), webapp.server)
         webapp.stop()
 
     request.addfinalizer(finalizer)
+
+    assert watcher.wait("ready", webapp.channel)
 
     return webapp
