1852a652c23bb4d62e092f1b8ddb7ea5ad15b961
James <j.mills@griffith.edu.au>
2013-01-30 11:57:40 +1000

    All tests passing again

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -2,22 +2,32 @@
 
 import pytest
 
+from os import path
 from urlparse import urljoin
-from operator import itemgetter
 
-from py import path
+from spyda import crawl
 
-from spyda import crawl, fetch_url, get_links
+
+@pytest.fixture()
+def sample_output(request, webapp):
+    return open(path.join(path.dirname(__file__), "output.txt"), "r").read().format(webapp.server.base)
+
+
+@pytest.fixture()
+def expected_links(request, webapp):
+    lines = (x.strip() for x in open(path.join(path.dirname(__file__), "links.txt"), "r"))
+    return sorted(x.format(webapp.server.base) for x in lines)
 
 
 def test_crawl(webapp, expected_links):
     result = crawl(webapp.server.base)
-    assert result["errors"] == set([(404, urljoin(webapp.server.base, "asdf/"))])
-    assert sorted(map(itemgetter(0), result["urls"])) == expected_links
+    print(repr(result["urls"]))
+    assert set(result["errors"]) == set([(404, urljoin(webapp.server.base, "asdf/"))])
+    assert sorted(result["urls"]) == expected_links
 
 
-def test_crawl_allowed_domains(webapp):
-    result = crawl(urljoin(webapp.server.base, "external"), allowed_domains=["localhost"])
+def test_crawl_allowed_urls(webapp):
+    result = crawl(urljoin(webapp.server.base, "external"), allowed_urls=["^http\:\/\/localhost.*"])
     assert not result["urls"]
     assert not result["errors"]
 
@@ -25,21 +35,21 @@ def test_crawl_allowed_domains(webapp):
 def test_crawl_max_depth(webapp):
     result = crawl(webapp.server.base, max_depth=1)
     assert not result["errors"]
-    assert sorted(map(itemgetter(0), result["urls"])) == ["asdf/", "foo/"]
+    assert sorted(result["urls"]) == list(x.format(webapp.server.base) for x in ("{0:s}/asdf/", "{0:s}/foo/"))
 
 
 def test_crawl_patterns(webapp):
     result = crawl(webapp.server.base, patterns=["^.*\/foo\/$"])
-    assert result["errors"] == set([(404, urljoin(webapp.server.base, "asdf/"))])
-    assert sorted(map(itemgetter(0), result["urls"])) == ["foo/"]
+    assert set(result["errors"]) == set([(404, urljoin(webapp.server.base, "asdf/"))])
+    assert sorted(result["urls"]) == list(x.format(webapp.server.base) for x in ("{0:s}/foo/",))
 
 
-def test_crawl_verbose(webapp, expected_links, capsys):
+def test_crawl_verbose(webapp, expected_links, sample_output, capsys):
     result = crawl(webapp.server.base, verbose=True)
-    assert result["errors"] == set([(404, urljoin(webapp.server.base, "asdf/"))])
-    assert sorted(map(itemgetter(0), result["urls"])) == expected_links
+    assert set(result["errors"]) == set([(404, urljoin(webapp.server.base, "asdf/"))])
+    assert sorted(result["urls"]) == expected_links
 
-    expected_output = SAMPLE_VERBOSE_OUTPUT.format(base_url=webapp.server.base)
+    expected_output = sample_output.format(base_url=webapp.server.base)
 
     out, err = capsys.readouterr()
-    assert out == expected_output
+    assert err == expected_output
