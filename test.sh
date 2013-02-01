f2243749ce407e1f799c0e1def12edbe39cfaf12
James <j.mills@griffith.edu.au>
2013-02-01 13:28:17 +1000

    ...

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+#spyda -p "https?\:\/\/app\.griffith\.edu\.au\/news\/[0-9]+\/[0-9]+\/[0-9]+\/.*" -a "https?\:\/\/app\.griffith\.edu\.au\/news\/[0-9]+\/[0-9]+\/.*"  http://app.griffith.edu.au/news/archives
+
 extract -f "title=#title-block h1" -f "content=article .entry-content" index.html | ./test_calais.py
