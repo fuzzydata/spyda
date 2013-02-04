d42f2f3059cdb24eaae3d53263f4ab2f1384b83e
James <j.mills@griffith.edu.au>
2013-02-04 16:11:20 +1000

    Updated test script with latest working invocation of both crawl and extract

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-#spyda -p "https?\:\/\/app\.griffith\.edu\.au\/news\/[0-9]+\/[0-9]+\/[0-9]+\/.*" -a "https?\:\/\/app\.griffith\.edu\.au\/news\/[0-9]+\/[0-9]+\/.*"  http://app.griffith.edu.au/news/archives
+#crawl -p "https?\:\/\/app\.griffith\.edu\.au\/news\/[0-9]+\/[0-9]+\/[0-9]+\/.*" -a "https?\:\/\/app\.griffith\.edu\.au\/news\/[0-9]+\/[0-9]+\/.*" http://app.griffith.edu.au/news/archives
 
-extract -f "title=#title-block h1" -f "content=article .entry-content" index.html | ./test_calais.py
+#extract -f "title=#title-block h1" -f "content=article .entry-content" index.html | ./test_calais.py
+
+crawl -p "https?\:\/\/app\.griffith\.edu\.au\/news\/[0-9]+\/[0-9]+\/[0-9]+\/.*" -a "https?\:\/\/app\.griffith\.edu\.au\/news\/[0-9]+\/[0-9]+\/.*" http://app.griffith.edu.au/news/archives -d 5 | extract -v -j 4 -f "title=#title-block h1" -f "content=article .entry-content" -o ./tmp --calais --calais-key="xgaxkzq96ep5rfjsptg4h3fm" -
