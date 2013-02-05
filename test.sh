379b2d1b7ffbddd2c8e5880321e80db13827fd46
James <j.mills@griffith.edu.au>
2013-02-06 09:23:27 +1000

    Removed testing scripts and samples

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#crawl -p "https?\:\/\/app\.griffith\.edu\.au\/news\/[0-9]+\/[0-9]+\/[0-9]+\/.*" -a "https?\:\/\/app\.griffith\.edu\.au\/news\/[0-9]+\/[0-9]+\/.*" http://app.griffith.edu.au/news/archives
-
-#extract -f "title=#title-block h1" -f "content=article .entry-content" index.html | ./test_calais.py
-
-crawl -p "https?\:\/\/app\.griffith\.edu\.au\/news\/[0-9]+\/[0-9]+\/[0-9]+\/.*" -a "https?\:\/\/app\.griffith\.edu\.au\/news\/[0-9]+\/[0-9]+\/.*" http://app.griffith.edu.au/news/archives -d 5 | extract -v -j 4 -f "title=#title-block h1" -f "content=article .entry-content" -o ./tmp --calais --calais-key="xgaxkzq96ep5rfjsptg4h3fm" -
--- test_extract.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-
-from spyda import parse_html
-
-
-html = open(sys.argv[1], "r").read()
-doc = parse_html(html)
