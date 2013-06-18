bc48dd05a64d42085154c76a91353df5d681f315
James Mills <j.mills@griffith.edu.au>
2013-06-18 11:19:18 +1000

    Tidied up setup.py for packaing

--- setup.py
+++ setup.py
@@ -1,50 +1,35 @@
 #!/usr/bin/env python
 
-import os
 from glob import glob
-from distutils.util import convert_path
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
+import spyda as pkg
 
-def find_packages(where=".", exclude=()):
-    """Borrowed directly from setuptools"""
-
-    out = []
-    stack = [(convert_path(where), "")]
-    while stack:
-        where, prefix = stack.pop(0)
-        for name in os.listdir(where):
-            fn = os.path.join(where, name)
-            if ("." not in name and os.path.isdir(fn) and
-                    os.path.isfile(os.path.join(fn, "__init__.py"))):
-                out.append(prefix + name)
-                stack.append((fn, prefix + name + "."))
-
-    from fnmatch import fnmatchcase
-    for pat in list(exclude) + ["ez_setup"]:
-        out = [item for item in out if not fnmatchcase(item, pat)]
-
-    return out
-
-path = os.path.abspath(os.path.dirname(__file__))
-try:
-    README = open(os.path.join(path, "README.rst")).read()
-    RELEASE = open(os.path.join(path, "RELEASE.rst")).read()
-except IOError:
-    README = RELEASE = ""
 
 setup(
-    name="spyda",
-    version="0.0.2dev",
-    description="Python Spider Tool(s) and Library",
-    long_description="%s\n\n%s" % (README, RELEASE),
+    name=pkg.__name__,
+    version=pkg.__version__,
+    description=pkg.__doc__.split("\n")[0],
+    long_description="{0:s}\n\n{1:s}".format(
+        open("README.rst").read(), open("RELEASE.rst").read()
+    ),
     author="James Mills",
     author_email="James Mills, j dot mills at griffith dot edu dot au",
-    url="TBA",
-    download_url="TBA",
-    classifiers=[],
-    license="TBA",
+    url="https://bitbucket.org/prologic/spyda",
+    download_url="https://bitbucket.org/prologic/spyda/downloads/get/tip.zip"
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+    ],
+    license="MIT",
     keywords="Python Spider Web Crawling and Extraction Tool and Library",
     platforms="POSIX",
     packages=find_packages("."),
